27f234e38d86d4ba3564bf8d46c9143bff7254ee
alieniskucuk-glitch <alieniskucuk@gmail.com>
2026-03-30 06:10:07 +0300

    login

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ import userUpdate from "./routes/user/update.js";
 import userFullCreat from "./routes/user/fullcreat.js";
 import userEdit from "./routes/user/edit.js";
 import userPremium from "./routes/user/premium.js";
-import userRefresh from "./routes/user/refresh.js"; // 🔥 EKLENDİ
 
 const app = express();
 
@@ -59,7 +58,6 @@ app.use("/user", userUpdate);
 app.use("/user", userFullCreat);
 app.use("/user", userEdit);
 app.use("/user", userPremium);
-app.use("/user", userRefresh); // 🔥 EKLENDİ
 
 /* =========================
    SERVER
